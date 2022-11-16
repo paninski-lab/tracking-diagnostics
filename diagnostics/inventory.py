81b52d2b983624cc2388676fac567a706fdba59b
danbider <danbider@gmail.com>
2022-11-16 17:55:39 -0500

    new query edits

--- diagnostics/inventory.py
+++ diagnostics/inventory.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 import pandas as pd
 import os
 import omegaconf
-from typing import List, Literal
+from typing import List, Literal, Union
 import yaml
 
 
@@ -61,6 +61,8 @@ class ModelInventoryBuilder:
             flat_dict = unroll_to_dict(cfg_) # flatten hierarchy
             df_ = pd.DataFrame([flat_dict]) # make as df
             time_created = time.ctime(os.path.getctime(config_file)) # get time created
+            # print time created before and after timestamp
+            # print(time_created)
             df_["timestamp"] = pd.Timestamp(time_created) # add timestamp
             df_["path"] = self.model_paths[i] # add path to artifacts
             self.df = pd.concat([self.df, df_]) # add to total df
@@ -69,25 +71,35 @@ class ModelInventoryBuilder:
             self.df["model.losses_to_use"] = self.df["model.losses_to_use"].apply(remove_quotes)
         
         return self.df
-    
-    from typing import List, Literal, Union
- 
+     
 class QueryBuilder:
     """ Useful string operations to build pandas queries that obey hydra + pandas + lightning syntax """
     def __init__(self, df: pd.DataFrame):
         self.df = df
         self.query_list = []
+        # self.value_list = [] # we need this for @ operators
     
     # each query has a parameter name, condition, and value or list of values
     def add_query(self, parameter_name: str, \
         condition: Literal[">", "<", "==", "!=", "in", "not in"], 
         value: str) -> None:
+
+        # self.value_list.append(value)
         
         # make sure that people don't err with adding double quotes
-        if parameter_name == "model.losses_to_use":
+        if parameter_name == "model.losses_to_use" and isinstance(value, str):
             value = remove_quotes(value)
-
-        self.query_list.append(f"`{parameter_name}` {condition} '{value}'")
+        
+        if condition == "in" or condition == "not in":
+            # assert that value is a list of strs
+            assert isinstance(value, list) and all([isinstance(v, str) for v in value]), "value must be a list of strings"
+            if condition == "in":
+                # this line manually implements the @ operator
+                self.query_list.append(" or ".join([f"`{parameter_name}` == '{v}'" for v in value]))
+                print(self.query_list[-1])          
+        # note the use of single quotes for the value and backticks for the parameter name which includes periods
+        else: 
+            self.query_list.append(f"`{parameter_name}` {condition} '{value}'")
     
     def add_timestamp_query(self, start_date: str, end_date: str) -> None:
         if start_date is not None and end_date is not None:
@@ -104,6 +116,6 @@ class QueryBuilder:
         # add operator between each query str
         # currently supports a single combination operator
         return (" %s " % operator).join([f"({q})" for q in self.query_list])
-    
+        
     def get_query(self) -> pd.DataFrame:
         return self.df.query(self.query_dict)
